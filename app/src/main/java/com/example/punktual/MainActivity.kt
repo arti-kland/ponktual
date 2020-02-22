993669e903fe384bc88e1234239b7b30bef17ab1
cultureweb <cedric.milius@yahoo.fr>
2020-02-22 15:28:58 +0100

    wip ced

--- app/src/main/java/com/example/punktual/MainActivity.kt
+++ app/src/main/java/com/example/punktual/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.punktual
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.MainActivityBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+        val binding = DataBindingUtil.inflate<MainActivityBinding>(inflater,
+            R.layout.activity_main,container,false)
+        //The complete onClickListener with Navigation
+        binding.playButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+
 }
